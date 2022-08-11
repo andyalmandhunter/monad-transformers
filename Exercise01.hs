32d63ffbbe8b4bd8877feb08b6762e9a4c9fb284
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2022-08-11 02:49:57 -0400

    Exercise 1

--- Exercise01.hs
+++ Exercise01.hs
@@ -1,10 +1,29 @@
--- Does not compile
 #!/usr/bin/env stack
 -- stack script --resolver lts-19.18 --package transformers
 {-# LANGUAGE DeriveFunctor #-}
-import           Control.Monad.IO.Class
-import           Control.Monad.Trans.Class
-import           Data.Functor.Identity
+
+import           Control.Monad.IO.Class         ( MonadIO(liftIO) )
+import           Control.Monad.Trans.Class      ( MonadTrans(lift) )
+import           Data.Functor.Identity          ( Identity(runIdentity) )
+
+newtype ReaderT r m a = ReaderT { runReaderT :: r -> m a }
+  deriving Functor
+
+instance Monad m => Applicative (ReaderT r m) where
+    pure a = ReaderT $ \_ -> return a
+    ReaderT f <*> ReaderT a = ReaderT $ \r -> f r <*> a r
+
+instance Monad m => Monad (ReaderT r m) where
+    return = pure
+    ReaderT a >>= f = ReaderT $ \r -> do
+        b <- a r
+        (runReaderT $ f b) r
+
+instance MonadTrans (ReaderT r) where
+    lift a = ReaderT $ const a
+
+instance MonadIO m => MonadIO (ReaderT r m) where
+    liftIO = lift . liftIO
 
 type Reader r = ReaderT r Identity
 
@@ -12,7 +31,8 @@ runReader :: Reader r a -> r -> a
 runReader r = runIdentity . runReaderT r
 
 ask :: Monad m => ReaderT r m r
-ask = _
+ask = ReaderT $ \r -> do
+    return r
 
 main :: IO ()
 main = runReaderT main' "Hello World"
@@ -21,4 +41,5 @@ main' :: ReaderT String IO ()
 main' = do
     lift $ putStrLn "I'm going to tell you a message"
     liftIO $ putStrLn "The message is:"
-    message <- ask lift $ putStrLn message
+    message <- ask
+    lift $ putStrLn message
